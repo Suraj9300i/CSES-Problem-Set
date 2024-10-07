0b9d551c28ee3d26eb06bd839ce746853a0557f3
suraj gupta <srjgupta17@gmail.com>
2024-10-07 10:15:52 +0530

    striver cp sheet

---
+++ Striver CP Sheet/144A.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+#define ff first
+#define ss second
+#define ll long long
+
+using namespace std;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+
+const int mod = 1e9 + 7;
+
+void fastIO(bool useFiles = false) {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    if (useFiles) {
+        #ifndef ONLINE_JUDGE 
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt","w",stdout);
+        #endif
+    }
+}
+
+// Debugging utilities
+template<class T> void _print(T x) { cerr << x; }
+template<class T> void _print(vector<T> v) { cerr << "[ "; for(T ele : v) cerr << ele << " "; cerr << "]\n"; }
+template<class T> void _print(T arr[], int n) { cerr << "[ "; for (int i = 0; i < n; i++) cerr << arr[i] << " "; cerr << "]\n"; }
+template<class T1, class T2> void _print(pair<T1, T2> p) { cerr << "{" << p.first << ", " << p.second << "} "; }
+template<class T> void _print(set<T> v) { cerr << "[ "; for (T ele : v) cerr << ele << " "; cerr << "]"; }
+template<class T1, class T2> void _print(map<T1, T2> m) { cerr << "[ "; for (auto p : m) cerr << "{" << p.first << ", " << p.second << "} "; cerr << "]"; }
+
+
+void solve() {
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    int mxi = 0, mni = 0;
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    int cnt  = 0;
+    for(int i=0; i<n; i++){
+        if(arr[i] > arr[mxi]) mxi = i;
+        if(arr[i] <= arr[mni]) mni = i;
+    }
+    
+    if(mxi < mni) cnt = (mxi + (n - 1 - mni));
+    else cnt = (mxi + (n - 1 - mni) - 1);
+    cout<<cnt;
+    
+}
+
+int main() {
+    fastIO(true);
+    int t = 1;
+    // cin >> t;
+    for (int i = 1; i <= t; i++) {
+        solve();
+    }
+    return 0;
+}
+
+
+
